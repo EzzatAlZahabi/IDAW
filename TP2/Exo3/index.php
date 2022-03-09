40f9153721aef9ab034e1f335da5116bd411ebba
EzzatAlZahabi <ezzatalzahabi@gmail.com>
2022-03-09 11:36:20 +0100

    m

---
+++ TP2/Exo3/index.php
@@ -0,0 +1,20 @@
+<?php
+    require_once('template_header.php');
+    require_once('template_menu.php');
+    renderMenuToHTML('index');
+?>
+        <div>
+            <h2>Objectif</h2>
+            <p>Recherche un stage de fin d’études en tant que Business Manager à partir du 11 avril 2022.</p>
+        </div>
+        <div>
+            <h2>Contact</h2>
+            <ul>
+                <li>Numéro de téléphone : 06 16 11 17 31</li>
+                <li>E-mail : ezzatalzahabi@hotmail.com</li>
+                <li>LinkedIn : Ezzat AL ZAHABI</li>
+                <li>Adresse : 32 Avenue de Lauterbourg, 69160 TASSIN LA DEMI LUNE</li>
+            </ul>
+        </div>
+        </body>
+</html>
