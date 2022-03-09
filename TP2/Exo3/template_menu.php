db7ca782df00d816a52460d8ba12cd6441e4b475
EzzatAlZahabi <ezzatalzahabi@gmail.com>
2022-03-09 09:46:02 +0100

    o

--- TP2/Exo3/template_menu.php
+++ TP2/Exo3/template_menu.php
@@ -1,15 +1,19 @@
         <?php
             function renderMenuToHTML($currentPageId) {
-            // un tableau qui d\'efinit la structure du site
-            $mymenu = array( // idPage titre
-            'index' => array( 'Accueil' ),
-            'cv' => array( 'Cv' ),
-            'projets' => array('Mes Projets')
+            $mymenu = array(
+            'accueil' => array( 'Accueil' ),
+            'competences' => array( 'Compétences' ),
+            'formations' => array( 'Formations' ),
+            'experiences' => array('Expériences')
+            'activites' => array( 'Activités extrascolaires' ),
             );
-            // ...
+            echo "<nav class=\"menu\"><ul>";
             foreach($mymenu as $pageId => $pageParameters) {
-            echo "...";
+                echo "<li><a ";
+                if ($currentPageId == $pageId)
+                    echo "id=\"$pageId\" ";
+                echo "href=\"$pageId.php\">$pageParameters[0]</a></li>\n";
             }
-            // ...
+            echo "</ul></nav>";
             }
         ?>
