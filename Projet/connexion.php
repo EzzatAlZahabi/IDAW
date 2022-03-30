a84cd01e02f5cca899a456334838b43d768b1b4f
EzzatAlZahabi <ezzatalzahabi@gmail.com>
2022-03-30 02:26:54 +0200

    c

--- Projet/connexion.php
+++ Projet/connexion.php
@@ -26,6 +26,9 @@
             echo "Votre login ou mot de passe est incorrect";
         }
     }
+    if(isset($_POST['inscription'])){
+        header('Location: inscription.php');
+    }
 ?>
 
 <form id="login_form" action="" method="POST">
@@ -39,8 +42,8 @@
             <td><input type="password" name="password" autocomplete="off"></td>
         </tr>
         <tr>
-            <th></th>
-            <td><button type="submit"  name="submit">Connexion</button></td>
+            <th><button type="submit"  name="submit">Connexion</button></th>
+            <td><button type="submit"  name="inscription">Inscription</button></td>
         </tr>
     </table>
 </form>
